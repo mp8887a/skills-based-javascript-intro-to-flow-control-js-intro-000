a87a7f8840b1b33ce7a07c52883f2c4b0c23b42f
Mike Pokalsky <mpoksky1989@gmail.com>
2020-06-18 15:44:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,8 @@
 function basicTeenager(age) {if (age>=13&&age<=19) {return "You are a teenager!";}
 }
 
-function teenager(age) {
+function teenager(age) {if (age>=13&&<=19) {return "You are a teenager!";} else {return "You are not a teenager!";}}
 
-}
 
 function ageChecker(age) {
 
